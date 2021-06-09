1b21ef29885f87586e45debd8b0fe2ebc54b7215
Julien Sulpis <julien.sulpis@gmail.com>
2021-06-09 20:36:25 +0200

    feat: initialize an empty app with Vite, Vue 3 and Typescript

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+body {
+  text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Metronome</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HelloWorld",
+});
+</script>
+
+<style scoped></style>
