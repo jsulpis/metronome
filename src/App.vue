94171062c673ba86284dc4a645598f7f01878b74
Julien Sulpis <julien.sulpis@gmail.com>
2021-07-22 19:41:03 +0200

    feat: add the progress track

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,11 @@
   <main>
     <h1>Metronome</h1>
     <div class="content-push">
-      <TempoSection class="tempo" />
-      <div class="progress-track">
-        <svg id="progress-track" width="300" height="300" viewBox="0 0 300 300">
-          <circle
-            cx="150"
-            cy="150"
-            r="144"
-            fill="none"
-            stroke="var(--grey-70)"
-            stroke-width="12"
-            :stroke-dasharray="2 * Math.PI * 150"
-            :stroke-dashoffset="2 * Math.PI * 150 * (1 - 0.66)"
-            stroke-linecap="round"
-          ></circle>
-          <circle
-            cx="150"
-            cy="150"
-            r="144"
-            fill="none"
-            stroke="var(--primary)"
-            stroke-width="12"
-            :stroke-dasharray="2 * Math.PI * 150"
-            :stroke-dashoffset="2 * Math.PI * 150 * (1 - 0.66 * ((bpm - min) / (max - min)))"
-            stroke-linecap="round"
-          ></circle>
-        </svg>
-        <Wheel
-          class="wheel"
-          :value="bpm"
-          :min="min"
-          :max="max"
-          @change="commit('setBpmValue', $event)"
-        />
+      <TempoSection />
+      <ProgressTrack :progress="(bpm - min) / (max - min)" class="progress-track">
+        <Wheel :value="bpm" :min="min" :max="max" @change="commit('setBpmValue', $event)" />
         <PlayButton class="play-button" />
-      </div>
+      </ProgressTrack>
     </div>
   </main>
 </template>
@@ -45,11 +15,12 @@
 import { computed, defineComponent } from "vue";
 import PlayButton from "./components/PlayButton.vue";
 import TempoSection from "./components/TempoSection.vue";
+import ProgressTrack from "./components/ProgressTrack.vue";
 import Wheel from "./components/Wheel.vue";
 import { useStore } from "vuex";
 
 export default defineComponent({
-  components: { PlayButton, TempoSection, Wheel },
+  components: { PlayButton, TempoSection, Wheel, ProgressTrack },
   setup() {
     const { state, commit } = useStore();
     const bpm = computed(() => state.bpm);
@@ -73,49 +44,16 @@ main {
   min-height: 100vh;
 }
 
-.play-button,
-#progress-track {
+.progress-track {
+  position: relative;
+  margin-bottom: 25vh;
+  margin-top: 24px;
+}
+
+.play-button {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-}
-
-#progress-track {
-  width: 100%;
-  padding: 4px;
-  height: auto;
-}
-
-circle {
-  transform: rotate(146deg);
-  transform-origin: center;
-}
-
-.progress-track {
-  &::after {
-    content: "";
-    position: absolute;
-    pointer-events: none;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    border-radius: 50%;
-    box-shadow: inset -2px -4px 3px rgba(0, 0, 0, 0.05),
-      inset 6px 4px 12px rgba(255, 255, 255, 0.54), -3px -3px 15px rgba(255, 255, 255, 0.7),
-      4px 6px 20px rgba(0, 0, 0, 0.2);
-  }
-
-  position: relative;
-  margin-bottom: 25vh;
-  margin-top: 24px;
-  display: flex;
-  justify-content: center;
-
-  background: var(--grey-50);
-  border-radius: 50%;
-  padding: 20px;
-  display: inline-block;
 }
 </style>
---
+++ src/components/ProgressTrack.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <svg :width="diameter" :height="diameter" :viewBox="`0 0 ${diameter} ${diameter}`">
+      <circle
+        :cx="radius"
+        :cy="radius"
+        :r="radius - strokeWidth / 2"
+        fill="none"
+        stroke="var(--grey-70)"
+        :stroke-width="strokeWidth"
+        :stroke-dasharray="perimeter"
+        :stroke-dashoffset="perimeter * (1 - 0.66)"
+        stroke-linecap="round"
+      ></circle>
+      <circle
+        :cx="radius"
+        :cy="radius"
+        :r="radius - strokeWidth / 2"
+        fill="none"
+        stroke="var(--primary)"
+        :stroke-width="strokeWidth"
+        :stroke-dasharray="perimeter"
+        :stroke-dashoffset="perimeter * (1 - 0.66 * progress)"
+        stroke-linecap="round"
+      ></circle>
+    </svg>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    progress: {
+      type: Number,
+      required: true,
+      validator(value: number) {
+        return value >= 0 && value <= 1;
+      }
+    }
+  },
+  setup() {
+    const radius = ref(150);
+    const diameter = computed(() => 2 * radius.value);
+    const perimeter = computed(() => 2 * Math.PI * radius.value);
+    const strokeWidth = ref(12);
+
+    return {
+      radius,
+      diameter,
+      perimeter,
+      strokeWidth
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+svg {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  padding: 4px;
+  height: auto;
+}
+
+circle {
+  transform: rotate(146deg);
+  transform-origin: center;
+}
+
+div {
+  position: relative;
+  background: var(--grey-50);
+  border-radius: 50%;
+  padding: 20px;
+  display: inline-block;
+
+  &::after {
+    content: "";
+    position: absolute;
+    pointer-events: none;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 50%;
+    box-shadow: inset -2px -4px 3px rgba(0, 0, 0, 0.05),
+      inset 6px 4px 12px rgba(255, 255, 255, 0.54), -3px -3px 15px rgba(255, 255, 255, 0.7),
+      4px 6px 20px rgba(0, 0, 0, 0.2);
+  }
+}
+</style>
