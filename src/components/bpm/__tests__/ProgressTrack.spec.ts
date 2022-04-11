1b2d94ef25abd93e3e33d1b613b715ee3bace527
Julien Sulpis <julien.sulpis@gmail.com>
2022-04-11 23:12:02 +0200

    chore: clean dependencies

--- src/components/bpm/__tests__/ProgressTrack.spec.ts
+++ src/components/bpm/__tests__/ProgressTrack.spec.ts
@@ -1,34 +1,34 @@
-import { mount } from "@vue/test-utils";
 import ProgressTrack from "@/components/bpm/ProgressTrack.vue";
+import { render } from "@testing-library/vue";
 
 describe("ProgressTrack.vue", () => {
   it("should match snapshot with progress = 0", async () => {
-    const wrapper = mount(ProgressTrack, {
+    const { container } = render(ProgressTrack, {
       props: {
         progress: 0
       }
     });
 
-    expect(wrapper.element).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   it("should match snapshot with progress = 60", async () => {
-    const wrapper = mount(ProgressTrack, {
+    const { container } = render(ProgressTrack, {
       props: {
         progress: 0.6
       }
     });
 
-    expect(wrapper.element).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   it("should match snapshot with progress = 100", async () => {
-    const wrapper = mount(ProgressTrack, {
+    const { container } = render(ProgressTrack, {
       props: {
         progress: 1
       }
     });
 
-    expect(wrapper.element).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
